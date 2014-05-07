12560d76cb178e94701a82051128a32fc36f756c
Ning-Zhou <nathon.py@gmail.com>
2014-05-08 01:13:09 +0200

    Process exercise 1: Write 2 processes to pingpong a message M times.

--- proc1.erl
+++ proc1.erl
@@ -1,1 +1,16 @@
-% test push
+% exercise
+% Write a function which starts 2 processes, and sends a message M times forewards and backwards between them. 
+% After the messages have been sent the processes should terminate gracefully.
+
+-module(proc1).
+-export([pingpong_messages/1]).
+
+pingpong_messages(M)->
+    
+
+% callback function for process 1
+
+callback_proc1()->
+    receive 
+	{From, Msg} ->
+	    io:format("
